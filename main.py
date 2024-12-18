995494c097766fabb9517568ae8e622c7a1d5abb
Orion-himiko <prishah.shah0707@gmail.com>
2024-12-18 21:43:51 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+name="Penguin"
+age=14
+weight=42.5
+student=True
+
+print("Name: ",name)
+print("Data type of name: ", type(name))
+
+print('Age: ',age)
+print('Data type of age: ', type(age))
+
+print('Weight: ',weight)
+print("Data type of weight: ", type(weight))
+
+print("Is student: ", student)
+print("Data type of student: ", type(student))
+
+print('\n After Type Casting....')
+
+age=str(age)
+print(age)
+print("Data type of age: ", type(age))
+
+weight=int(weight)
+print(weight)
+print("Data type of weight: ", type(weight))
